93e45ea6ad6e549a873aee18341b2a8a355bc34b
Peter <vipekon@yandex.ru>
2022-08-02 00:44:30 +0300

    TB-0:Added Spring skeleton

---
+++ vipekon-telegrambot/src/main/java/com/github/vipekon/vipekontelegrambot/VipekonTelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.github.vipekon.vipekontelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipekonTelegrambotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipekonTelegrambotApplication.class, args);
+	}
+
+}
