5fa58a20984382f1cf845bc575d6d01b78e95503
vipekon <vipekon@yandex.ru>
2022-08-28 20:44:15 +0300

    TB-3 added Command pattern for handling Telegram Bot commands

---
+++ src/main/java/com/github/vipekon/vipekontelegrambot/bot/VipekonTelegramBot.java
@@ -0,0 +1,59 @@
+package com.github.vipekon.vipekontelegrambot.bot;
+
+
+import com.github.vipekon.vipekontelegrambot.command.CommandContainer;
+import com.github.vipekon.vipekontelegrambot.service.SendBotMessageServiceImpl;
+import com.github.vipekon.vipekontelegrambot.service.SendBotMessageService;
+import com.github.vipekon.vipekontelegrambot.service.SendBotMessageServiceImpl;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import static com.github.vipekon.vipekontelegrambot.command.CommandName.*;
+/**
+ * Telegram bot for Javarush Community from Javarush community.
+ */
+@Component
+public class VipekonTelegramBot extends TelegramLongPollingBot {
+
+    public static String COMMAND_PREFIX = "/";
+
+    @Value("${bot.username}")
+    private String username;
+
+    @Value("${bot.token}")
+    private String token;
+
+    private final CommandContainer commandContainer;
+
+    public VipekonTelegramBot () {
+        this.commandContainer = new CommandContainer(new SendBotMessageServiceImpl(this));
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText().trim();
+            if (message.startsWith(COMMAND_PREFIX)) {
+                String commandIdentifier = message.split(" ")[0].toLowerCase();
+
+                commandContainer.retrieveCommand(commandIdentifier).execute(update);
+            } else {
+                commandContainer.retrieveCommand(NO.getCommandName()).execute(update);
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+}
